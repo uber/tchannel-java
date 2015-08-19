66beb15fe24d6169225637adcd46a2b347f564aa
Will Salisbury <wjs@uber.com>
2015-08-19 14:47:51 -0700

    Run checkstyle on tchannel-hyperbahn

--- tchannel-core/src/test/java/com/uber/tchannel/headers/RetryFlagTest.java
+++ tchannel-core/src/test/java/com/uber/tchannel/headers/RetryFlagTest.java
@@ -29,9 +29,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class RetryFlagTest {
 
