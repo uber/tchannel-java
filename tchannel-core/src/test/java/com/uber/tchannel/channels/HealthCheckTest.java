1b6eb56560de52f65027bdc242fce0e456d8484c
Anson Qian <abq@uber.com>
2015-12-01 16:48:11 -0800

    Add health check test

---
+++ tchannel-core/src/test/java/com/uber/tchannel/channels/HealthCheckTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2015 Uber Technologies, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.uber.tchannel.channels;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.uber.tchannel.api.SubChannel;
+import com.uber.tchannel.api.TChannel;
+import com.uber.tchannel.messages.ThriftRequest;
+import com.uber.tchannel.messages.ThriftResponse;
+import com.uber.tchannel.messages.generated.HealthStatus;
+import com.uber.tchannel.messages.generated.Meta;
+import org.junit.Test;
+
+import java.net.InetAddress;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class HealthCheckTest {
+    @Test
+    public void testHealthCheck() throws Exception {
+
+        InetAddress host = InetAddress.getByName("127.0.0.1");
+
+        // create server
+        final TChannel server = new TChannel.Builder("server")
+                .setServerHost(host)
+                .build();
+        server.makeSubChannel("server");
+        server.listen();
+
+        int port = server.getListeningPort();
+
+        // create client
+        final TChannel client = new TChannel.Builder("client")
+                .setServerHost(host)
+                .build();
+        final SubChannel subClient = client.makeSubChannel("server");
+        client.listen();
+
+        ThriftRequest req = new ThriftRequest.Builder<Meta.health_args>("server", "Meta::health")
+                .setBody(new Meta.health_args())
+                .build();
+
+        ListenableFuture<ThriftResponse<Meta.health_result>> future = subClient.send(
+                req,
+                host,
+                port);
+
+        ThriftResponse<Meta.health_result> res = future.get();
+        assertFalse(res.isError());
+        HealthStatus status = res.getBody(Meta.health_result.class).getSuccess();
+        assertEquals(true, status.isOk());
+        res.release();
+
+        server.shutdown();
+    }
+}
