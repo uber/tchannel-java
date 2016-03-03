8f25610025b40eafeeff3eb7ed792eed9a16c431
Abhinav Gupta <abg@uber.com>
2016-03-03 14:22:19 -0800

    travis: don't use sudo for build
    Send the Thrift build to ~/thrift instead.

--- build/travis/install-thrift.sh
+++ build/travis/install-thrift.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
+set -x
+
+THRIFTDIR="$HOME/thrift"
+mkdir -p "$THRIFTDIR"
+
 wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
 tar -xzvf thrift-0.9.2.tar.gz
 cd thrift-0.9.2
-./configure --with-cpp=no --with-c_glib=no --with-java=yes --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
-make -j2 && sudo make install
-cd ..
+./configure \
+	--prefix="$THRIFTDIR" --with-cpp=no --with-c_glib=no --with-java=yes \
+	--with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
+make -j2 && make install
