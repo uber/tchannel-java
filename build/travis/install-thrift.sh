d1ccf2259b036cf9041a86bca6b94c00d2a97545
Abhinav Gupta <abg@uber.com>
2016-03-03 14:45:12 -0800

    testing

--- build/travis/install-thrift.sh
+++ build/travis/install-thrift.sh
@@ -2,13 +2,10 @@
 set -e
 set -x
 
-THRIFTDIR="$HOME/thrift"
-mkdir -p "$THRIFTDIR"
+mkdir -p "$THRIFT_PREFIX"
 
 wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
 tar -xzvf thrift-0.9.2.tar.gz
 cd thrift-0.9.2
-./configure \
-	--prefix="$THRIFTDIR" --with-cpp=no --with-c_glib=no --with-java=yes \
-	--with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
+./configure --prefix="$THRIFT_PREFIX" --enable-libs=no --enable-tests=no --enable-tutorial=no
 make -j2 && make install
