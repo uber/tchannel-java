72d06e841224e8ba64da0367dbccfe04746b064c
Alexander Shopov <ashopov@uber.com>
2017-04-04 13:18:36 +0200

    Extract thrift executable version as parameter

--- build/travis/install-thrift.sh
+++ build/travis/install-thrift.sh
@@ -2,10 +2,11 @@
 set -e
 set -x
 
+THRIFT_VER=0.9.2
 mkdir -p "$THRIFT_PREFIX"
 
-wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
-tar -xzvf thrift-0.9.2.tar.gz
-cd thrift-0.9.2
+wget http://archive.apache.org/dist/thrift/${THRIFT_VER}/thrift-${THRIFT_VER}.tar.gz
+tar -xzvf thrift-${THRIFT_VER}.tar.gz
+cd thrift-${THRIFT_VER}
 ./configure --prefix="$THRIFT_PREFIX" --enable-libs=no --enable-tests=no --enable-tutorial=no
 make -j2 && make install
