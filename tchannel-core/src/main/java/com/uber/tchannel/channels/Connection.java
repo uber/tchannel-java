6a56e70e41221f1aba2d070882ab674ddca9a13c
Alexander Shopov <ashopov@uber.com>
2017-11-13 21:13:18 +0100

    Hide lock of Connection
    Connection is meant to be publicly accessed.
    It can also be subclassed. Synchronisation lock should
    be hidden. If someone synchronises on Connection we will
    get into deadlock.
    
    This slightly changes the semantics of the class as it
    makes impossible for classes that are not inheriting from
    this to lock and be notified by it. However it is highly
    unlikely that such behaviour is right.

--- tchannel-core/src/main/java/com/uber/tchannel/channels/Connection.java
+++ tchannel-core/src/main/java/com/uber/tchannel/channels/Connection.java
@@ -40,6 +40,7 @@ import java.util.Map;
 public class Connection {
 
     private static final Logger logger = LoggerFactory.getLogger(Connection.class);
+    private static final String EPHEMERAL = "0.0.0.0:0";
 
     public Direction direction = Direction.NONE;
     public ConnectionState state = ConnectionState.UNCONNECTED;
@@ -48,6 +49,7 @@ public class Connection {
     private final @NotNull Channel channel;
     private @Nullable String remoteAddress = null;
     private @Nullable TChannelError lastError = null;
+    final protected @NotNull Object lock;
 
     public Connection(Peer peer, @NotNull Channel channel, Direction direction) {
         this.peer = peer;
@@ -56,64 +58,77 @@ public class Connection {
         if (channel.isActive() && this.state == ConnectionState.UNCONNECTED) {
             this.state = ConnectionState.CONNECTED;
         }
+        lock = new Object();
     }
 
     public @NotNull Channel channel() {
         return this.channel;
     }
 
-    public synchronized @Nullable TChannelError lastError() {
-        return this.lastError;
-    }
-
-    public synchronized boolean satisfy(@Nullable ConnectionState preferredState) {
-        ConnectionState connState = this.state;
-        if (connState == ConnectionState.DESTROYED) {
-            return false;
-        } else if (preferredState == null) {
-            return true;
-        } else if (connState == preferredState || connState == ConnectionState.IDENTIFIED) {
-            return true;
-        } else if (connState == ConnectionState.CONNECTED && preferredState == ConnectionState.UNCONNECTED) {
-            return true;
-        } else {
-            return false;
+    public @Nullable TChannelError lastError() {
+        synchronized (lock) {
+            return this.lastError;
         }
     }
 
-    public synchronized boolean satisfy(@Nullable Direction preferredDirection) {
-        return preferredDirection == null || preferredDirection == Direction.NONE || preferredDirection == direction;
-    }
-
-    public synchronized void setState(ConnectionState state) {
-        this.state = state;
-        if (state == ConnectionState.IDENTIFIED || (state == ConnectionState.UNCONNECTED && this.lastError != null)) {
-            this.notifyAll();
+    public boolean satisfy(@Nullable ConnectionState preferredState) {
+        synchronized (lock) {
+            ConnectionState connState = this.state;
+            if (connState == ConnectionState.DESTROYED) {
+                return false;
+            } else if (preferredState == null) {
+                return true;
+            } else if (connState == preferredState || connState == ConnectionState.IDENTIFIED) {
+                return true;
+            } else if (connState == ConnectionState.CONNECTED && preferredState == ConnectionState.UNCONNECTED) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
-    public synchronized void setIdentified(@NotNull Map<String, String> headers) {
-        String hostPort = headers.get(InitFrame.HOST_PORT_KEY);
-        // TODO: handle protocol error
-        this.remoteAddress = hostPort == null ? "0.0.0.0:0" : hostPort.trim();
-        this.setState(ConnectionState.IDENTIFIED);
+    public boolean satisfy(@Nullable Direction preferredDirection) {
+        synchronized (lock) {
+            return preferredDirection == null || preferredDirection == Direction.NONE || preferredDirection == direction;
+        }
+    }
+
+    public void setState(ConnectionState state) {
+        synchronized (lock) {
+            this.state = state;
+            if (state == ConnectionState.IDENTIFIED || (state == ConnectionState.UNCONNECTED && this.lastError != null)) {
+                lock.notifyAll();
+            }
+        }
+    }
+
+    public void setIdentified(@NotNull Map<String, String> headers) {
+        synchronized (lock) {
+            String hostPort = headers.get(InitFrame.HOST_PORT_KEY);
+            // TODO: handle protocol error
+            this.remoteAddress = hostPort == null ? EPHEMERAL : hostPort.trim();
+            this.setState(ConnectionState.IDENTIFIED);
+        }
     }
 
     /** @deprecated typo - use {@link #setIdentified(Map)} */
     @Deprecated
-    public synchronized void setIndentified(@NotNull Map<String, String> headers) {
-        setIdentified(headers);
+    public void setIndentified(@NotNull Map<String, String> headers) {
+            setIdentified(headers);
     }
 
     public synchronized void setIdentified(TChannelError error) {
-        this.remoteAddress = null;
-        this.lastError = error;
-        this.setState(ConnectionState.UNCONNECTED);
+        synchronized (lock) {
+            this.remoteAddress = null;
+            this.lastError = error;
+            this.setState(ConnectionState.UNCONNECTED);
+        }
     }
 
     /** @deprecated typo - use {@link #setIdentified(TChannelError)} */
     @Deprecated
-    public synchronized void setIndentified(TChannelError error) {
+    public void setIndentified(TChannelError error) {
         setIdentified(error);
     }
 
@@ -127,16 +142,22 @@ public class Connection {
         return isIdentified();
     }
 
-    public synchronized boolean isEphemeral() {
-        return "0.0.0.0:0".equals(this.remoteAddress);
+    public boolean isEphemeral() {
+        synchronized (lock) {
+            return EPHEMERAL.equals(this.remoteAddress);
+        }
     }
 
-    public synchronized @Nullable String getRemoteAddress() {
-        return this.remoteAddress;
+    public @Nullable String getRemoteAddress() {
+        synchronized (lock) {
+            return this.remoteAddress;
+        }
     }
 
-    public synchronized @NotNull SocketAddress getRemoteAddressAsSocketAddress() {
-        return hostPortToSocketAddress(this.remoteAddress);
+    public @NotNull SocketAddress getRemoteAddressAsSocketAddress() {
+        synchronized (lock) {
+            return hostPortToSocketAddress(this.remoteAddress);
+        }
     }
 
     public static @NotNull String[] splitHostPort(@NotNull String hostPort) {
@@ -154,37 +175,41 @@ public class Connection {
         return new InetSocketAddress(strs[0], Integer.parseInt(strs[1]));
     }
 
-    public synchronized boolean waitForIdentified(long timeout) {
-        // TODO reap connections/peers on init timeout
-        try {
-            if (this.state != ConnectionState.IDENTIFIED) {
-                this.lastError = null;
-                this.wait(timeout);
+    public boolean waitForIdentified(long timeout) {
+        synchronized (lock) {
+            // TODO reap connections/peers on init timeout
+            try {
+                if (this.state != ConnectionState.IDENTIFIED) {
+                    this.lastError = null;
+                    lock.wait(timeout);
+                }
+            } catch (InterruptedException ex) {
+                // doesn't matter if we got interrupted here ...
+                // set interrupt flag
+                Thread.currentThread().interrupt();
+                logger.warn("wait for identified is interrupted.", ex);
             }
-        } catch (InterruptedException ex) {
-            // doesn't matter if we got interrupted here ...
-            // set interrupt flag
-            Thread.currentThread().interrupt();
-            logger.warn("wait for identified is interrupted.", ex);
-        }
 
-        boolean result = this.state == ConnectionState.IDENTIFIED;
-        if (!result) {
-            // reset the connection if it failed to identify
-            this.clean();
-        }
+            boolean result = this.state == ConnectionState.IDENTIFIED;
+            if (!result) {
+                // reset the connection if it failed to identify
+                this.clean();
+            }
 
-        return result;
+            return result;
+        }
     }
 
-    public synchronized void close() {
-        ResponseRouter responseRouter = channel.pipeline().get(ResponseRouter.class);
-        if (responseRouter != null) {
-            responseRouter.clean();
-        }
+    public void close() {
+        synchronized (lock) {
+            ResponseRouter responseRouter = channel.pipeline().get(ResponseRouter.class);
+            if (responseRouter != null) {
+                responseRouter.clean();
+            }
 
-        channel.close();
-        this.state = ConnectionState.DESTROYED;
+            channel.close();
+            this.state = ConnectionState.DESTROYED;
+        }
     }
 
     public void clean() {
