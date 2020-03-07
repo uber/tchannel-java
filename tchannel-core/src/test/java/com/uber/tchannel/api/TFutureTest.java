b5c69a94c6fd1a75a7942b9e3dbbb5052060fa30
Denyska Kurylenko <denys@kurylenko.com>
2020-03-07 11:43:44 -0800

    compilation error fix

--- tchannel-core/src/test/java/com/uber/tchannel/api/TFutureTest.java
+++ tchannel-core/src/test/java/com/uber/tchannel/api/TFutureTest.java
@@ -15,6 +15,8 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 public class TFutureTest {
 
